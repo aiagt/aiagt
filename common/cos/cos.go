a01096864eefbffd1797097d962b4e6dbb5fa6a2
ahao <92586826+ahaostudy@users.noreply.github.com>
2024-12-12 20:31:45 +0800

    feat: support file type message (#32)

--- common/cos/cos.go
+++ common/cos/cos.go
@@ -1,25 +1,31 @@
 package cos
 
 import (
+	"github.com/aiagt/aiagt/pkg/logerr"
 	"github.com/tencentyun/cos-go-sdk-v5"
 	"net/http"
 	"net/url"
 )
 
-var (
-	Cli *cos.Client
-)
+var cli *cos.Client
+
+func Cli() *cos.Client {
+	return cli
+}
 
 const (
 	AvatarDir     = "avatar"
 	AppLogoDir    = "app_logo"
 	PluginLogoDir = "plugin_logo"
+	ChatFile      = "chat_file"
 )
 
 func InitCos(cosURL, secretID, secretKey string) {
-	u, _ := url.Parse(cosURL)
-	b := &cos.BaseURL{BucketURL: u}
-	Cli = cos.NewClient(b, &http.Client{
+	u, err := url.Parse(cosURL)
+	logerr.Fatal(err)
+
+	bucket := &cos.BaseURL{BucketURL: u}
+	cli = cos.NewClient(bucket, &http.Client{
 		Transport: &cos.AuthorizationTransport{
 			SecretID:  secretID,
 			SecretKey: secretKey,
