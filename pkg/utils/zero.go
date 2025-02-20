14e3bf83cbc48f21b161ab574ebc03167bd66457
ahaostudy <1993584108@qq.com>
2025-02-20 23:17:14 +0800

    fix: tool_calls refund empty index

--- pkg/utils/zero.go
+++ pkg/utils/zero.go
@@ -9,3 +9,12 @@ func Zero[T any]() T {
 func IsZero[T comparable](v T) bool {
 	return v == Zero[T]()
 }
+
+func NonZero[T comparable](v T) bool {
+	return v != Zero[T]()
+}
+
+func NonZeroAndNotEqual[T comparable](a, b T) bool {
+	zero := Zero[T]()
+	return a != zero && b != zero && a != b
+}
