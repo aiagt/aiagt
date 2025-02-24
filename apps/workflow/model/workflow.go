5ac0635bb3e16dc0b35071390131a1f875cbae6a
ahaostudy <1993584108@qq.com>
2025-02-24 23:45:13 +0800

    fix: utils.PrettyBytes method

--- apps/workflow/model/workflow.go
+++ apps/workflow/model/workflow.go
@@ -1,11 +1,17 @@
 package model
 
+import "time"
+
 type Workflow struct {
 	Base
-	Name        string `gorm:"type:varchar(255);not null" json:"name"`
-	Description string `gorm:"type:varchar(255);not null" json:"description"`
 
-	Nodes []WorkflowNode `gorm:"-"`
+	Name          string     `gorm:"column:name;type:varchar(255);not null"`
+	Description   string     `gorm:"column:description"`
+	DescriptionMd string     `gorm:"column:description_md;type:text"`
+	AuthorID      int64      `gorm:"column:author_id"`
+	IsPrivate     bool       `gorm:"column:is_private"`
+	Logo          string     `gorm:"column:logo;type:varchar(255);not null"`
+	PublishedAt   *time.Time `gorm:"column:published_at"`
 }
 
 type WorkflowOptional struct {
